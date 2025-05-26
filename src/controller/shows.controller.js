5ac2dfbae2faabbaa6de06bc41482a5f98dee2be
Ayush Raj <ayushraj2482@gmail.com>
2025-05-26 15:49:34 +0530

    added shows and details routes

--- src/controller/shows.controller.js
+++ src/controller/shows.controller.js
@@ -1,23 +1,85 @@
 const retryRequest = require("../utils/retryRequest")
 
-const getShowsList = async (req,res) => {
+const getShowsList = async (req, res) => {
     try {
 
-        const {title,type} = req.params
-        console.log(title);
+        const { title, type } = req.params
         const config = {
-            method:'GET',
-            url:`/${type}/${title}`
+            method: 'GET',
+            url: `/${type}/${title}`
         }
         const response = await retryRequest(config);
         res.send(response.data)
-        
+
     } catch (error) {
         console.log(error);
-        
+
     }
 }
 
 
+const getNowPlaying = async (req, res) => {
+    try {
+        const config = {
+            method: 'GET',
+            url: "/movie/now_playing"
+        }
+        const response = await retryRequest(config);
+        res.send(response.data)
+    } catch (error) {
+        console.log(error);
 
-module.exports={getShowsList}
+    }
+}
+
+
+const getMovieTrailer = async (req, res) => {
+
+    try {
+        const { id } = req.params
+        const config = {
+            method: 'GET',
+            url: `/movie/${id}/videos`
+        }
+        const response = await retryRequest(config);
+        res.send(response.data)
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+
+const getCategoryShowList = async (req, res) => {
+    try {
+        const { page } = req.query;
+        const { type, category } = req.params
+        const config = {
+            method: 'GET',
+            url: `/${type}/${category}`,
+            params: {
+                page: page
+            }
+        }
+        const response = await retryRequest(config);
+        res.send(response.data)
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+
+const getSingleShowDetail = async (req, res) => {
+    try {
+        const { type, movieId } = req.params;
+        const config = {
+            method: "GET",
+            url: `/${type}/${movieId}`
+        }
+        const response = await retryRequest(config);
+        res.send(response.data)
+    } catch (error) {
+
+    }
+}
+
+module.exports = { getShowsList, getMovieTrailer, getNowPlaying, getCategoryShowList, getSingleShowDetail }
--- src/routes/shows.route.js
+++ src/routes/shows.route.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const showsController = require("../controller/shows.controller.js")
+//homepage trailer api      
+router.route("/movie/now_playing").get(showsController.getNowPlaying);
+router.route("/movie/:id/videos").get(showsController.getMovieTrailer);
+
+
+router.route("/listOfShowCategory/:type/:category").get(showsController.getCategoryShowList);
+router.route("/singleShowDetails/:type/:movieId").get(showsController.getSingleShowDetail);
+
 
 router.route("/:type/:title").get(showsController.getShowsList);
+
 
 
 module.exports= router;
